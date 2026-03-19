8fbac1d923cd17d1e43360bb458583d10c5efcda
tamasolah17 <65872186+tamasolah17@users.noreply.github.com>
2026-03-19 05:53:17 +0100

    My agent

--- static/widget.js
+++ static/widget.js
@@ -15,7 +15,7 @@ window.addEventListener("DOMContentLoaded", function () {
     button.style.fontSize = "26px";
     button.style.position = "fixed";
     button.style.bottom = "20px";
-    button.style.right = "490px";
+    button.style.right = "20px";
      button.style.width = "60px";
     button.style.height = "60px";
     button.style.borderRadius = "50%";
@@ -39,7 +39,7 @@ window.addEventListener("DOMContentLoaded", function () {
     const chatBox = document.createElement("div");
     chatBox.style.position = "fixed";
     chatBox.style.bottom = "90px";
-    chatBox.style.right = "350px";
+    chatBox.style.right = "20px";
     chatBox.style.width = "350px";
     chatBox.style.height = "460px";
     chatBox.style.background = "white";
