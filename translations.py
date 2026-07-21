9360401c1d4d88cbec117f6e8c6eb4f639b2ad36
tamasolah17 <nnavocs@gmail.com>
2026-07-21 16:25:36 +0200

    My agent

--- translations.py
+++ translations.py
@@ -2,7 +2,7 @@ TRANSLATIONS = {
 
     "de": {
 
-        "welcome": "Hallo und herzlich willkommen in unserer Praxis 😊 Ich bin Ihr digitaler Assistent. Wie kann ich Ihnen helfen?",
+        "welcome": "Hallooo und herzlich willkommen in unserer Praxis 😊 Ich bin Ihr digitaler Assistent. Wie kann ich Ihnen helfen?",
 
         "treatments": {
             "whitening": "Zahnaufhellung",
